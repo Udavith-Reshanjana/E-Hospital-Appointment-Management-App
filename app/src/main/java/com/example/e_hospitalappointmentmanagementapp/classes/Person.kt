032454e74c715ad2edaaa22bd0aa522b47d0f78a
Udavith Reshanjana <146712079+Udavith-Reshanjana@users.noreply.github.com>
2024-12-26 13:57:42 +0530

    backend update for doctor registration

--- app/src/main/java/com/example/e_hospitalappointmentmanagementapp/classes/Person.kt
+++ app/src/main/java/com/example/e_hospitalappointmentmanagementapp/classes/Person.kt
@@ -1,7 +1,10 @@
 package com.example.e_hospitalappointmentmanagementapp.classes
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
+import java.sql.Blob
 
 open class Person(private val context: Context) {
     protected val dbHelper: SQLiteOpenHelper = DatabaseConnection(context)
@@ -47,4 +50,58 @@ open class Person(private val context: Context) {
             -2 // Return -2 in case of an error
         }
     }
+
+    // person registration function
+    fun signUpPerson(
+        firstName: String,
+        lastName: String?,
+        personEmail: String,
+        personPassword: String,
+        profilePic: ByteArray?,
+        role: Int,
+        docSpecialty: String?,
+        medicalLicence: ByteArray?,
+        age: Int?
+    ): Boolean {
+        return try {
+            dbHelper.writableDatabase.use { db ->
+                val values = ContentValues().apply {
+                    put("FIRST_NAME", firstName)
+                    put("LAST_NAME", lastName)
+                    put("EMAIL", personEmail)
+                    put("PERSON_PASSWORD", personPassword)
+                    put("PROFILE_PIC", profilePic)
+                    put("ROLE_TYPE", role)
+                    put("DOC_SPECIALITY", docSpecialty)
+                    put("MEDICAL_LICENCE", medicalLicence)
+                    put("AGE", age)
+                }
+                val rowId = db.insert("PERSON", null, values)
+                rowId != -1L // Returns true if insertion was successful
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
+
+    fun isEmailValid(email: String): Boolean {
+        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+        return !email.matches(emailPattern.toRegex())
+    }
+
+    fun isEmailExists(email: String): Boolean {
+        return try {
+            dbHelper.readableDatabase.use { db ->
+                val query = "SELECT 1 FROM PERSON WHERE EMAIL = ?"
+                db.rawQuery(query, arrayOf(email)).use { cursor ->
+                    cursor.moveToFirst() // Returns true if a record is found
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
+
 }
