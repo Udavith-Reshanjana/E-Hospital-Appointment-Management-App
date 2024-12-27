c0424f1ed7c2072b1c8b11309db57677fdd46453
Udavith Reshanjana <146712079+Udavith-Reshanjana@users.noreply.github.com>
2024-12-27 13:08:21 +0530

    update

--- app/src/main/java/com/example/e_hospitalappointmentmanagementapp/classes/Doctor.kt
+++ app/src/main/java/com/example/e_hospitalappointmentmanagementapp/classes/Doctor.kt
@@ -4,21 +4,5 @@ import android.content.Context
 
 class Doctor(context: Context) : Person(context) {
 
-    fun getDoctorFirstName(userEmail: String, userPassword: String): String? {
-        return try {
-            dbHelper.readableDatabase.use { db ->
-                val query = "SELECT FIRST_NAME FROM PERSON WHERE EMAIL = ? AND PERSON_PASSWORD = ?"
-                db.rawQuery(query, arrayOf(userEmail, userPassword)).use { cursor ->
-                    if (cursor.moveToFirst()) {
-                        cursor.getString(cursor.getColumnIndexOrThrow("FIRST_NAME"))
-                    } else {
-                        null
-                    }
-                }
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            null
-        }
-    }
+
 }
--- app/src/main/java/com/example/e_hospitalappointmentmanagementapp/classes/Person.kt
+++ app/src/main/java/com/example/e_hospitalappointmentmanagementapp/classes/Person.kt
@@ -137,4 +137,23 @@ open class Person(private val context: Context) {
         }
     }
 
+    // person first name getter
+    fun getPersonFirstName(userEmail: String, userPassword: String): String? {
+        return try {
+            dbHelper.readableDatabase.use { db ->
+                val query = "SELECT FIRST_NAME FROM PERSON WHERE EMAIL = ? AND PERSON_PASSWORD = ?"
+                db.rawQuery(query, arrayOf(userEmail, userPassword)).use { cursor ->
+                    if (cursor.moveToFirst()) {
+                        cursor.getString(cursor.getColumnIndexOrThrow("FIRST_NAME"))
+                    } else {
+                        null
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+
 }
